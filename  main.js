af16d057ff500d2cf130d9783e18f11338e99adc
juliabuenosantos30-create <julia.bueno.santos30@escola.pr.gov.br>
2025-10-28 07:46:06 -0300

    hug

---  main.js
+++  main.js
@@ -5,7 +5,9 @@ avanca.forEach(button => {
     button.addEventListener('click' , function () {
        const atual = document.querySelector(' , ativo')
        const proximoPasso = 'passo.' + this.getArtribute('data-proximo')
-       
+
+       atual.classList.remove('ativo')
+       document.getElementById(proximoPasso).classList.add ('ativo')
 
     })
 })
